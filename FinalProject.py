8498b1374a55fede20c3a7f30c70120d35f31d8e
Diwash Ban <dban1@terpmail.umd.edu>
2023-04-04 16:15:53 -0400

    added __init__ method to the FinalProject.py

--- FinalProject.py
+++ FinalProject.py
@@ -2,3 +2,19 @@
 
 class Polynomial: 
     """stores information about the polynomial and its derivative"""
+    
+    def __init__(self,coefficents,x,y):
+        """Initalizing coefficents, x-interval and y-interval attributes
+            
+            Args:
+                 coefficents(list): list of coefficents 
+                 x(tuple): values of x-interval 
+                 y(tuple): values of  y-interval
+                 
+        """
+        
+        self.coefficents = coefficents
+        self.x = x
+        self.y = y
+        
+        
